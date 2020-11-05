4c687a71fe235c74e0556c0fd498d2ad9b805eb7
hensn5250 <hensn5250@users.noreply.github.com>
2020-11-05 05:23:42 -0500

    Update script.js
    Added more descriptive comments  for each function.

--- script.js
+++ script.js
@@ -9,21 +9,22 @@ new WOW().init();
   A plugin used to automatically update links in a navigation list based on scroll position.
 */
 	$(document).ready(function() {
-		// Navigation click actions	
+		// Navigation click actions
 		$('.scroll-link').on('click', function(event) {
 			event.preventDefault();
-			let sectionID = $(this).attr("data-id");	//assign ID of 'this' object to variable
-			scrollToID('#' + sectionID, 750);			//scroll to section in 750ms
+			let sectionID = $(this).attr("data-id");  // Assign ID of 'this' object to variable
+			scrollToID('#' + sectionID, 750);  // Scroll to section in 750ms
     });
     
 		// Scroll to top action
-		$('.scroll-top').on('click', function(event) {	//click eventlistener for class
+		$('.scroll-top').on('click', function(event) {
 			event.preventDefault();		
 			$('html, body').animate({scrollTop:0}, 'slow'); 		
 		});
 	});
-	// Scroll function
-	function scrollToID(id, speed){
+
+  // Scroll function
+	function scrollToID(id, speed) {
 		let offSet = 50;
 		let targetOffset = $(id).offset().top - offSet;
 		let mainNav = $('#navbar_menu');
@@ -32,7 +33,8 @@ new WOW().init();
 			mainNav.css("height", "1px").removeClass("in").addClass("collapse");
 			mainNav.removeClass("open");
 		}
-	}
+  }
+  
 	if (typeof console === "undefined") {
 	    console = {
 	        log: function() { }
